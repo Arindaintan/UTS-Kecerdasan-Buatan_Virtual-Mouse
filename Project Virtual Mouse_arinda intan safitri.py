c2a5fa142984ce7024c47e449117192399b64e33
Arindaintan <72563376+Arindaintan@users.noreply.github.com>
2022-10-04 12:33:55 +0700

    Add files via upload

---
+++ Project Virtual Mouse_arinda intan safitri.py
@@ -0,0 +1,238 @@
+#Virtual Mouse adalah perangkat lunak yg memungkinkan pengguna untuk memberikan input mouse ke sistem tanpa menggunakan mouse yg sebenarnya.
+#Secara ekstrim bisa juga disebut sebagai hardware karena menggunakan web camera biasa.
+#Sebuah mouse virtual biasanya dapat dioperasikan dengan beberapa perangkat input yg mungkin termasuk mouse yg sebenarnya atau keyboard komputer.
+#Mouse virtual yang menggunakan kamera web bekerja dengan bantuan teknik pemrosesan gambar yang berbeda.
+print ("================================= ARINDA INTAN SAFITRI ================================");
+print ("==================================== 09011282025041 ===================================");
+print ("===================================== VIRTUAL MOUSE ===================================");
+
+# Imports
+
+import cv2 
+import mediapipe as mp  
+import pyautogui
+import math
+from enum import IntEnum
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+from google.protobuf.json_format import MessageToDict
+import screen_brightness_control as sbcontrol
+
+pyautogui.FAILSAFE = False
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+# Pengkodean Gerakan
+class Gest(IntEnum):
+    # Kode Biner
+    FIST = 0
+    PINKY = 1
+    RING = 2
+    MID = 4
+    LAST3 = 7
+    INDEX = 8
+    FIRST2 = 12
+    LAST4 = 15
+    THUMB = 16    
+    PALM = 31
+    
+    # Pemetaan Ekstra
+    V_GEST = 33
+    TWO_FINGER_CLOSED = 34
+    PINCH_MAJOR = 35
+    PINCH_MINOR = 36
+
+# Label Multi-tangan
+class HLabel(IntEnum):
+    MINOR = 0
+    MAJOR = 1
+
+# Konversikan Landmark Mediapipe ke Gerakan yang dapat dikenali
+class HandRecog:
+    
+    def __init__(self, hand_label):
+        self.finger = 0
+        self.ori_gesture = Gest.PALM
+        self.prev_gesture = Gest.PALM
+        self.frame_count = 0
+        self.hand_result = None
+        self.hand_label = hand_label
+    
+    def update_hand_result(self, hand_result):
+        self.hand_result = hand_result
+
+    def get_signed_dist(self, point):
+        sign = -1
+        if self.hand_result.landmark[point[0]].y < self.hand_result.landmark[point[1]].y:
+            sign = 1
+        dist = (self.hand_result.landmark[point[0]].x - self.hand_result.landmark[point[1]].x)**2
+        dist += (self.hand_result.landmark[point[0]].y - self.hand_result.landmark[point[1]].y)**2
+        dist = math.sqrt(dist)
+        return dist*sign
+    
+    def get_dist(self, point):
+        dist = (self.hand_result.landmark[point[0]].x - self.hand_result.landmark[point[1]].x)**2
+        dist += (self.hand_result.landmark[point[0]].y - self.hand_result.landmark[point[1]].y)**2
+        dist = math.sqrt(dist)
+        return dist
+    
+    def get_dz(self,point):
+        return abs(self.hand_result.landmark[point[0]].z - self.hand_result.landmark[point[1]].z)
+    
+    # Berfungsi untuk menemukan Pengkodean Gerakan menggunakan finger_state saat ini.
+    # Finger_state: 1 jika jari terbuka, kalau tidak 0
+    def set_finger_state(self):
+        if self.hand_result == None:
+            return
+
+        points = [[8,5,0],[12,9,0],[16,13,0],[20,17,0]]
+        self.finger = 0
+        self.finger = self.finger | 0 #jempol/ibu jari
+        for idx,point in enumerate(points):
+            
+            dist = self.get_signed_dist(point[:2])
+            dist2 = self.get_signed_dist(point[1:])
+            
+            try:
+                ratio = round(dist/dist2,1)
+            except:
+                ratio = round(dist1/0.01,1)
+
+            self.finger = self.finger << 1
+            if ratio > 0.5 :
+                self.finger = self.finger | 1
+    
+
+    # Menangani Flukteasi karena kebisingan
+    def get_gesture(self):
+        if self.hand_result == None:
+            return Gest.PALM
+
+        current_gesture = Gest.PALM
+        if self.finger in [Gest.LAST3,Gest.LAST4] and self.get_dist([8,4]) < 0.05:
+            if self.hand_label == HLabel.MINOR :
+                current_gesture = Gest.PINCH_MINOR
+            else:
+                current_gesture = Gest.PINCH_MAJOR
+
+        elif Gest.FIRST2 == self.finger :
+            point = [[8,12],[5,9]]
+            dist1 = self.get_dist(point[0])
+            dist2 = self.get_dist(point[1])
+            ratio = dist1/dist2
+            if ratio > 1.7:
+                current_gesture = Gest.V_GEST
+            else:
+                if self.get_dz([8,12]) < 0.1:
+                    current_gesture =  Gest.TWO_FINGER_CLOSED
+                else:
+                    current_gesture =  Gest.MID
+            
+        else:
+            current_gesture =  self.finger
+        
+        if current_gesture == self.prev_gesture:
+            self.frame_count += 1
+        else:
+            self.frame_count = 0
+
+        self.prev_gesture = current_gesture
+
+        if self.frame_count > 4 :
+            self.ori_gesture = current_gesture
+        return self.ori_gesture
+
+# Menjalankan perintah sesuai dengan gerakan yang terdeteksi
+class Controller:
+    tx_old = 0
+    ty_old = 0
+    trial = True
+    flag = False
+    grabflag = False
+    pinchmajorflag = False
+    pinchminorflag = False
+    pinchstartxcoord = None
+    pinchstartycoord = None
+    pinchdirectionflag = None
+    prevpinchlv = 0
+    pinchlv = 0
+    framecount = 0
+    prev_hand = None
+    pinch_threshold = 0.3
+    
+    def getpinchylv(hand_result):
+        dist = round((Controller.pinchstartycoord - hand_result.landmark[8].y)*10,1)
+        return dist
+
+    def getpinchxlv(hand_result):
+        dist = round((hand_result.landmark[8].x - Controller.pinchstartxcoord)*10,1)
+        return dist
+    
+    def changesystembrightness():
+        currentBrightnessLv = sbcontrol.get_brightness()/100.0
+        currentBrightnessLv += Controller.pinchlv/50.0
+        if currentBrightnessLv > 1.0:
+            currentBrightnessLv = 1.0
+        elif currentBrightnessLv < 0.0:
+            currentBrightnessLv = 0.0       
+        sbcontrol.fade_brightness(int(100*currentBrightnessLv) , start = sbcontrol.get_brightness())
+    
+    def changesystemvolume():
+        devices = AudioUtilities.GetSpeakers()
+        interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+        volume = cast(interface, POINTER(IAudioEndpointVolume))
+        currentVolumeLv = volume.GetMasterVolumeLevelScalar()
+        currentVolumeLv += Controller.pinchlv/50.0
+        if currentVolumeLv > 1.0:
+            currentVolumeLv = 1.0
+        elif currentVolumeLv < 0.0:
+            currentVolumeLv = 0.0
+        volume.SetMasterVolumeLevelScalar(currentVolumeLv, None)
+    
+    def scrollVertical():
+        pyautogui.scroll(120 if Controller.pinchlv>0.0 else -120)
+        
+    
+    def scrollHorizontal():
+        pyautogui.keyDown('shift')
+        pyautogui.keyDown('ctrl')
+        pyautogui.scroll(-120 if Controller.pinchlv>0.0 else 120)
+        pyautogui.keyUp('ctrl')
+        pyautogui.keyUp('shift')
+
+    # Temukan Tangan untuk mendapatkan Posisi Kursor
+    # Menstabilkan kursor dengan Peredam
+    def get_position(hand_result):
+        point = 9
+        position = [hand_result.landmark[point].x ,hand_result.landmark[point].y]
+        sx,sy = pyautogui.size()
+        x_old,y_old = pyautogui.position()
+        x = int(position[0]*sx)
+        y = int(position[1]*sy)
+        if Controller.prev_hand is None:
+            Controller.prev_hand = x,y
+        delta_x = x - Controller.prev_hand[0]
+        delta_y = y - Controller.prev_hand[1]
+
+        distsq = delta_x**2 + delta_y**2
+        ratio = 1
+        Controller.prev_hand = [x,y]
+
+        if distsq <= 25:
+            ratio = 0
+        elif distsq <= 900:
+            ratio = 0.07 * (distsq ** (1/2))
+        else:
+            ratio = 2.1
+        x , y = x_old + delta_x*ratio , y_old + delta_y*ratio
+        return (x,y)
+
+    def pinch_control_init(hand_result):
+        Controller.pinchstartxcoord = hand_result.landmark[8].x
+        Controller.pinchstartycoord = hand_result.landmark[8].y
+        Controller.pinchlv = 0
+        Controller.prevpinchlv = 0
+        Controller.framecount = 0
+
+ 
